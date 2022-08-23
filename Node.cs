af9172679751962654dfd473a02cc96543831757
lingesh2836 <sharanraj982@gmail.com>
2022-08-23 22:15:44 +0530

    Binary Search

---
+++ Node.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace BST
+{
+    class BinaryNode<T> where T : IComparable<T>
+    {
+        T NodeData;
+        public BinaryNode<T> leftTree { get; set; }
+        public BinaryNode<T> rightTree { get; set; }
+
+        bool result = false;
+        int leftCount = 0;
+        int rightCount = 0;
+
+        public BinaryNode(T NodeData)
+        {
+            this.NodeData = NodeData;
+            leftTree = null;
+            rightTree = null;
+        }
+        public void Insert(T item)
+        {
+            T currentNodeValue = NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (leftTree == null)
+                    leftTree = new BinaryNode<T>(item);
+                else
+                    leftTree.Insert(item);
+            }
+            else
+            {
+                if (rightTree == null)
+                    rightTree = new BinaryNode<T>(item);
+                else
+                    rightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (leftTree != null)
+            {
+                leftTree.Display();
+            }
+            Console.WriteLine(NodeData.ToString());
+            if (rightTree != null)
+            {
+                rightTree.Display();
+            }
+        }
+        public void Size()
+        {
+            Console.WriteLine(" \nSize of BST is " + (1 + leftCount + rightCount) + "\n");
+        }
+        public bool ifExists(T element, BinaryNode<T> node)
+        {
+            if (node == null)
+                return false;
+            if (node.NodeData.Equals(element))
+            {
+                Console.WriteLine("\n(UC-3), Element searched in BST :" + node.NodeData + "\n");
+                return true;
+            }
+            else
+                Console.WriteLine("(UC-2), Element {0} is added in BST ", node.NodeData);
+            if (element.CompareTo(node.NodeData) < 0)
+                ifExists(element, node.leftTree);
+            if (element.CompareTo(node.NodeData) > 0)
+                ifExists(element, node.rightTree);
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree! \n");
+            Console.WriteLine("UC-1:");
+            BinaryNode<int> binarySearchTree = new BinaryNode<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Insert(22);
+            binarySearchTree.Insert(40);
+            binarySearchTree.Insert(60);
+            binarySearchTree.Insert(95);
+            binarySearchTree.Insert(11);
+            binarySearchTree.Insert(65);
+            binarySearchTree.Insert(3);
+            binarySearchTree.Insert(16);
+            binarySearchTree.Insert(63);
+            binarySearchTree.Insert(67);
+            binarySearchTree.Display();
+            binarySearchTree.Size();
+            bool findElement = binarySearchTree.ifExists(63, binarySearchTree);
+            Console.ReadKey();
+        }
+    }
+}
